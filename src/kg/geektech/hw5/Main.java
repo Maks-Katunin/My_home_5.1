0b62f6e80e526916357c66a9fdc104d2eef763f4
Maks-Katunin <katunin87@gmail.com>
2021-08-14 17:40:04 +0600

    Add new package. Extra lines removed.

--- src/kg/geektech/hw5/Main.java
+++ src/kg/geektech/hw5/Main.java
@@ -10,21 +10,8 @@ public class Main {
         boss.setBossDefence("magic");
 
         System.out.println("Boss health: (" + boss.getBossHealth() + ")" +
-                " Boss damage: [" + boss.getBossDamage() + "] " + "Boss defence is " + boss.getBossDefence() + ".");
-
+                " Boss damage: [" + boss.getBossDamage() + "] " +
+                "Boss defence is " + boss.getBossDefence() + ".");
     }
-
-
-
- /*   public Hero createHeroes;
-        Hero hero1 = new Hero(250, 20, "kinetic");
-        Hero hero2 = new Hero(260, 25, "Medic");
-        Hero hero3 = new Hero(270, 15);
-
-        Hero[] array = {hero1, hero2, hero3};
-
-        return = ;*/
-
-
 }
 
