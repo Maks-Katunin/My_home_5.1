6b234862113f2aa5cab067404954894d71fc1e18
Maks-Katunin <katunin87@gmail.com>
2021-08-15 23:42:53 +0600

    Add new package. Extra lines removed. add createHeroes.

--- src/kg/geektech/hw5/Main.java
+++ src/kg/geektech/hw5/Main.java
@@ -1,5 +1,7 @@
 package kg.geektech.hw5;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,6 +17,35 @@ public class Main {
 
                 "Boss defence is " + boss.getBossDefence() + ".");
 
+        createHeroes();
+    }
+
+    public static Hero createHeroes() {
+
+        Hero hero1 = new Hero(250, 15);
+
+        System.out.println("Hero1 helth " + hero1.getHeroHealth() + ":" +
+                "  damage " + "[" + hero1.getHeroDamage() + "]");
+
+        Hero hero2 = new Hero(260, 20, "Magic");
+
+        System.out.println("Hero2 helth " + hero2.getHeroHealth() + ":" +
+                "  damage " + "[" + hero2.getHeroDamage() + "]" +
+                "  defence is " + hero2.getHeroDefence() + ".");
+
+        Hero hero3 = new Hero(270, 25, "Medic");
+
+        System.out.println("Hero3 helth " + hero3.getHeroHealth() + ":" +
+                "  damage " + "[" + hero3.getHeroDamage() + "]" +
+                "  defence is " + hero3.getHeroDefence() + ".");
+
+
+        int[][] createHeroes = {{hero1.getHeroHealth(), hero1.getHeroDamage()},
+                {hero2.getHeroHealth(), hero2.getHeroDamage()},
+                {hero3.getHeroHealth(), hero3.getHeroDamage()}};
+        System.out.println(Arrays.deepToString(createHeroes));
+
+        return hero1;
     }
 
 }
